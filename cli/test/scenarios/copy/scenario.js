ea0cd942444e229e2a46c6dfa7e4aeafdf5b6c0b
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-11-20 10:17:30 +0100

    Try reverse dirs order

--- cli/test/scenarios/copy/scenario.js
+++ cli/test/scenarios/copy/scenario.js
@@ -1,12 +1,12 @@
 module.exports = {
   init: [
-    {ino: 1, path: 'dir1/'},
-    {ino: 2, path: 'dir1/empty-subdir/'},
-    {ino: 3, path: 'dir1/subdir/'},
-    {ino: 4, path: 'dir1/subdir/file'}
+    {ino: 1, path: 'dir2/'},
+    {ino: 2, path: 'dir2/empty-subdir/'},
+    {ino: 3, path: 'dir2/subdir/'},
+    {ino: 4, path: 'dir2/subdir/file'}
   ],
   actions: [
-    {type: 'copy', src: 'dir1', dst: 'dir2'}
+    {type: 'copy', src: 'dir2', dst: 'dir1'}
   ],
   expected: {
     prepCalls: [],
